a36dd98cf382a0271d22869d5d88af9f6365f4a8
mewanDimalsha <e19111@eng.pdn.ac.lk>
2026-04-05 17:30:51 +0530

    feat: implement product and variant management with TypeORM and DTO validation

--- mo-marketplace-api/src/products/products.module.ts
+++ mo-marketplace-api/src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Variant } from './entities/variant.entity';
 import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Variant])],
+  providers: [ProductsService],
   controllers: [ProductsController],
-  providers: [ProductsService]
 })
-export class ProductsModule {}
+export class ProductsModule { }
